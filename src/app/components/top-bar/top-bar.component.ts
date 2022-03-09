7fe96a74db6fb55ce1b019e9be0d0da79f4eb9bf
Dilan Garcia <dilanngcia@gmail.com>
2022-03-08 17:48:03 -0700

    top-bar styles improved

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -7,7 +7,11 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class TopBarComponent implements OnInit {
 
-  public listMeats = ["a", "b", "c"];
+  public listMeats = ["Cambiar de cuenta...", "Pasar a modo desconectado...", "Comprobar si existen actualizacion del cliente de steam...", "Hacer copia de serguradad y restaurar prpgramas...", "Parámetros", "Salir"];
+  public listVer = ["Blblioteca", "Juegos ocultos", "Descargas", "Modo reducido", "Modo Big Picture", "Reproductor de música", "Amigos", "Jugadores", "Servidores", "Capturas", "Inventario", "Noticias de actualizaciones", "Parámetros"];
+  public listFriends = ["Ver lista de amigos", "Añadir a un amigo...", "Cambiar nombre de perfil / avatar...", "Conectado", "Ausente", "Invisible", "Desconectado"];
+  public listProducts = ["Ver biblioteca de productos", "Activar un producto en Meats...", "Canjear un código de la Cartera de Meats", "Gestionar regalos y pases de invitado...", "Añadir un producto que no es de Meats a mi biblioteca..."];
+  public listHelp = ["Soporte de Meats", "Política de Privacidad", "Información legal", "Acuerdo de Suscriptor a Meats", "Información del sistema", "Acerca de Meats"];
   public active = "";
   public clicked = false;
   constructor() { }
